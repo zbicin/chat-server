9f37f91c87380295ea4b43670a366658de338ce3
Krzysztof Zbicinski <1648189+zbicin@users.noreply.github.com>
2019-11-22 20:03:19 +0100

    POST /login

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const app = express()
 const port = 3000
+
+app.use(cors())
+
+const jsonParser = bodyParser.json()
+
+const users = []
+
+app.post('/login', jsonParser, (req, res) => {
+  const user = {
+    login: req.body.login
+  }
+  users.push(user);
+  console.log(`User ${user.login} joined`);
+  return res.json(user);
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
