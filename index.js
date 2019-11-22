35f59fd193a1833db115d37b2ca18e32eb9c537e
Krzysztof Zbicinski <1648189+zbicin@users.noreply.github.com>
2019-11-22 19:59:51 +0100

    Hello World express app

--- index.js
+++ index.js
@@ -1,38 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
 const app = express()
 const port = 3000
-
-app.use(cors())
-
-const jsonParser = bodyParser.json()
-
-const messages = []
-
-const users = []
-
-app.post('/login', jsonParser, (req, res) => {
-  const user = {
-    login: req.body.login
-  }
-  users.push(user);
-  return res.json(user);
-})
-
-app.post('/sendMessage', jsonParser, (req, res) => {
-  const message = {
-    body: req.body.message,
-    sentBy: req.body.login
-  }
-  messages.push(message)
-  return res.json(message);
-})
-
-app.get('/messages', (req, res) => {
-  return res.json(messages)
-})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
