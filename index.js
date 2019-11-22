f5763ba179c88b12f411dabf1ba9f662e8daad10
Krzysztof Zbicinski <1648189+zbicin@users.noreply.github.com>
2019-11-22 20:15:32 +0100

    Add  --prompt command line switch

--- index.js
+++ index.js
@@ -2,49 +2,67 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const etag = require('etag')
+const readline = require('readline');
 
-const app = express()
-const port = 3000
+const defaultPort = 3000;
 
-app.use(cors())
+if (process.argv.includes('--prompt')) {
+  const rl = readline.createInterface(process.stdin, process.stdout);
+  rl.question('Which port should I use? ', (answer) => {
+    if (isNaN(answer)) {
+      console.warn(`Invalid port. Falling back to default: ${defaultPort}`);
+      runServer(defaultPort);
+    } else {
+      runServer(answer);
+    }
+  });
+} else {
+  runServer(defaultPort);
+}
 
-const jsonParser = bodyParser.json()
+function runServer(port) {
+  const app = express()
 
-const users = []
+  app.use(cors())
 
-const messages = [
-  { body: 'test message', sentBy: 'test user' }
-]
+  const jsonParser = bodyParser.json()
 
-app.post('/login', jsonParser, (req, res) => {
-  const user = {
-    login: req.body.login
-  }
-  users.push(user);
-  console.log(`User ${user.login} joined`);
-  return res.json(user);
-})
+  const users = []
 
-app.post('/sendMessage', jsonParser, (req, res) => {
-  const message = {
-    body: req.body.message,
-    sentBy: req.body.login
-  }
-  messages.push(message)
-  return res.json(message);
-})
+  const messages = [
+    { body: 'test message', sentBy: 'test user' }
+  ]
 
-app.get('/messages', (req, res) => {
-  res.setHeader('ETag', etag(JSON.stringify(messages)));
-  return res.json(messages)
-})
+  app.post('/login', jsonParser, (req, res) => {
+    const user = {
+      login: req.body.login
+    }
+    users.push(user);
+    console.log(`User ${user.login} joined`);
+    return res.json(user);
+  })
 
-app.head('/messages', (req, res) => {
-  res.setHeader('ETag', etag(JSON.stringify(messages)));
-  res.end();
-})
+  app.post('/sendMessage', jsonParser, (req, res) => {
+    const message = {
+      body: req.body.message,
+      sentBy: req.body.login
+    }
+    messages.push(message)
+    return res.json(message);
+  })
 
-app.get('/', (req, res) => res.send('Hello World!'))
+  app.get('/messages', (req, res) => {
+    res.setHeader('ETag', etag(JSON.stringify(messages)));
+    return res.json(messages)
+  })
+
+  app.head('/messages', (req, res) => {
+    res.setHeader('ETag', etag(JSON.stringify(messages)));
+    res.end();
+  })
+
+  app.get('/', (req, res) => res.send('Hello World!'))
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
